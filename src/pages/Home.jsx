96f5ae9b0520c0517a51f358e74f949f8d2e5699
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2023-01-29 10:24:17 -0700

    creating grid

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,51 @@
+import "./Home.css"
 const Home = () => {
     return (
         <>
-            <h1>Home</h1>
+        <div className="container">
+            <div className="cell">1</div>
+            <div className="cell">2</div>
+            <div className="cell">3</div>
+            <div className="cell">4</div>
+            <div className="cell">5</div>
+            <div className="cell">6</div>
+            <div className="cell">7</div>
+            <div className="cell">8</div>
+            <div className="cell">9</div>
+            <div className="cell">10</div>
+            <div className="cell">11</div>
+            <div className="cell">12</div>
+            <div className="cell">13</div>
+            <div className="cell">14</div>
+            <div className="cell">15</div>
+            <div className="cell">16</div>
+            <div className="cell">17</div>
+            <div className="cell">18</div>
+            <div className="cell">19</div>
+            <div className="cell">20</div>
+            <div className="cell">21</div>
+            <div className="cell">22</div>
+            <div className="cell">23</div>
+            <div className="cell">24</div>
+            <div className="cell">25</div>
+            <div className="cell">26</div>
+            <div className="cell">27</div>
+            <div className="cell">28</div>
+            <div className="cell">29</div>
+            <div className="cell">30</div>
+            <div className="cell">31</div>
+            <div className="cell">32</div>
+            <div className="cell">33</div>
+            <div className="cell">34</div>
+            <div className="cell">35</div>
+            <div className="cell">36</div>
+            <div className="cell">37</div>
+            <div className="cell">38</div>
+            <div className="cell">39</div>
+            <div className="cell">40</div>
+            <div className="cell">41</div>
+            <div className="cell">42</div>
+        </div> 
         </>
     )
 }
