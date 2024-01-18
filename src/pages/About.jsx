1192d8658072e1e4d3c3ed0902d33b2c3aaf4989
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2024-01-18 15:58:42 -0700

    write about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,22 @@
+import "./About.css";
+
 const About = () => {
-    return (
-        <>
-            <h1>About</h1>
-        </>
-    )
-}
+  return (
+    <div className="about">
+      <h2>About</h2>
+      <h4>What is this</h4>
+      <p>
+        It's Connect4! You place your coins into the columns and try to get four
+        in a row before the other player. It's pretty fun!
+      </p>
+      <h4>Why I made this</h4>
+      <p>
+        I created this game to practice my react and css skills. As well as my
+        ability to come up with an algorithm that could check to see if someone
+        has won the game.
+      </p>
+    </div>
+  );
+};
 
 export default About;
